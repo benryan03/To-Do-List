cc68b494a1a27f4bdf17195897f1b5d99ec40dca
benryan03 <50218096+benryan03@users.noreply.github.com>
2020-02-29 17:14:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,13 @@
-var task1_strikethrough = false;
-var task2_strikethrough = false;
-var task3_strikethrough = false;
+function task_clicked(task_num){
 
-
-function task1_clicked(){
-
-    if (task1_strikethrough == false) {
-        document.getElementById("task1").style.textDecoration = "line-through";
-        task1_strikethrough = true;
+    var text_status = document.getElementById("task" + task_num).style.textDecoration
+    
+    if (text_status == ""){
+        document.getElementById("task" + task_num).style.textDecoration = "line-through";
     }
-
-    else {
-        document.getElementById("task1").style.textDecoration = "none";
-        task1_strikethrough = false;
+    
+    else if (text_status == "line-through"){
+        document.getElementById("task" + task_num).style.textDecoration = "none";
     }
-
-}
-
-function task2_clicked(){
-
-    if (task2_strikethrough == false) {
-        document.getElementById("task2").style.textDecoration = "line-through";
-        task2_strikethrough = true;
-    }
-
-    else {
-        document.getElementById("task2").style.textDecoration = "none";
-        task2_strikethrough = false;
-    }
-
-}
-
-function task3_clicked(){
-
-    if (task3_strikethrough == false) {
-        document.getElementById("task3").style.textDecoration = "line-through";
-        task3_strikethrough = true;
-    }
-
-    else {
-        document.getElementById("task3").style.textDecoration = "none";
-        task3_strikethrough = false;
-    }
-
+    
 }
